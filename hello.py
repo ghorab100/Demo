a9905413cb99b85bb5e2b0da5eb7138240e76a15
ghorab100 <inuse5409@gmail.com>
2025-05-25 19:06:18 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,13 +5,13 @@ name = input("What is your name? ")
 age = input("How old are you? ")
 email = input("Please insert a valid email: ")
 address = input("What is your address? ")
-
+bool Married = input("Are you marrid ?")
 # Define the function
-def print_info(Name, age, email, address):
+def print_info(Name, age, email, address , bool married):
     print("Hello, " + Name + "! Nice to meet you.")
     print("My Age: " + age + ".")
     print("My address is: " + address + ".")
     print("Your Email address is: " + email + ".")
-
+    print("Your status is : " + married + ".")
 # Call the function
-print_info(name, age, email, address)
+print_info(name, age, email, address,Married)
