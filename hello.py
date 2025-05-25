6c8fa06393fe87a15718d684d6ce78e7125807dc
Ghorab <inuse5409@gmail.com>
2025-05-25 18:45:57 +0300

    Editing hello.py python file adding new var.

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@
 
 # Ask the user for their name
 name = input("What is your name? ")
-
+Age = input("How old are you?")
 # Print a greeting
 print("Hello, " + name + "! Nice to meet you.")
+
+print("My Age : , " + Age + ".")
+
+
