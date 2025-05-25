7ebb701d914ed567ed4aee089d1df0baa2fa5f5e
Ghorab <inuse5409@gmail.com>
2025-05-25 18:47:41 +0300

    adding another var called address to hello.py

--- hello.py
+++ hello.py
@@ -3,9 +3,12 @@
 # Ask the user for their name
 name = input("What is your name? ")
 Age = input("How old are you?")
+Address = input("What is you address ?")
 # Print a greeting
 print("Hello, " + name + "! Nice to meet you.")
 
 print("My Age : , " + Age + ".")
+
+print("My address is : " + Address + "." );
 
 
