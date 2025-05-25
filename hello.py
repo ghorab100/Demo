f88ee6ed4c63a0d1a0bae0e0aadb8a143c4ca4f9
ghorab100 <inuse5409@gmail.com>
2025-05-25 18:54:11 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,13 +3,20 @@
 # Ask the user for their name
 name = input("What is your name? ")
 Age = input("How old are you?")
+Email = input("Please insert a valid Email !")
 Address = input("What is you address ?")
 # Print a greeting
-print("Hello, " + name + "! Nice to meet you.")
+def Print_Info(Name , Age , email , Address)
+
+print("Hello, " + Name + "! Nice to meet you.")
 
 print("My Age : , " + Age + ".")
 
 print("My address is : " + Address + "." )
 
+print("Your Email address is : " + email + ".")
+
+
+Print_Info(name,Age,Email,Address)
 
 
